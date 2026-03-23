13c391507d01f670b8bb2471594258c37eba0979
devgabs1 <gabriel.fe132@gmail.com>
2026-03-23 20:44:45 -0300

    Projeto finalizado

---
+++ src/main/java/com/gabrielfems/biblioteca_sec/BibliotecaSecApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielfems.biblioteca_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielfems/biblioteca_sec/LivroSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfems.biblioteca_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
